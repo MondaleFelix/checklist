863b615d86047dcd98fc28a7c3404ab6fe04a5e9
MondaleFelix <mondalefelix@yahoo.com>
2019-09-01 00:59:39 -0700

    added crud functions

---
+++ checklist.py
@@ -0,0 +1,38 @@
+checklist = []
+
+# CREATE
+def create(item):
+    checklist.append(item)
+
+ # READ
+def read(index):
+    return checklist[index]
+
+# UPDATE
+def update(index, item):
+    checklist[index] = item
+
+# DESTROY
+def destroy(index):
+    checklist.pop(index)
+
+def list_all_items():
+    index = 0
+    for list_item in checklist:
+        print(str(index) + list_item)
+        index += 1
+
+def test():
+    create("purple sox")
+    create("red cloak")
+
+    print(read(0))
+    print(read(1))
+
+    update(0, "purple socks")
+
+    destroy(1)
+
+    print(read(0))
+
+    list_all_items()
