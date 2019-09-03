778058211a9bdd631b8e450d5bb0cbf8c6fdf454
MondaleFelix <mondalefelix@yahoo.com>
2019-09-02 19:31:17 -0700

    added edge cases

--- checklist.py
+++ checklist.py
@@ -23,44 +23,79 @@ def list_all_items():
         index += 1
 
 def mark_completed(index):
-    checklist[index] = "{} {}".format("√", checklist[index])
+    checklist[int(index)] = "{} {}".format("√", checklist[int(index)])
 
     # Add code here that marks an item as completed
 
 def select(function_code):
-    # Create item
-    if function_code == "C":
-        input_item = user_input("Input item:")
+    # Add item
+    if function_code == "C" or function_code == "c":
+        input_item = user_input("Add item: ")
         create(input_item)
+        list_all_items()
+    # Remove item
+    elif function_code == "D" or function_code == "d":
+        # remove from list
+        if len(checklist) > 0:
+            item_index = user_input("Enter index number: ")
+            destroy(item_index)
+            list_all_items()
+        else: 
+            print("Nothing to remove")
 
-    # Read item
-    elif function_code == "R":
-        item_index = user_input("Index Number?")
+    # Update item with user input
+    elif function_code == "U" or function_code == "u":
+        item_index = user_input("Enter index number: ")
+        input_item = user_input("Input Item: ")
+        update(item_index, input_item)
 
-        # Remember that item_index must actually exist or our program will crash.
-        read(item_index)
-
-    # Print all items
-    elif function_code == "P":
+    # Mark as complete
+    elif function_code == "M" or function_code == "m":
+        item_index = user_input("Enter index number: ")
+        mark_completed(item_index)
         list_all_items()
 
-    # Catch all
+    # Print all items
+    elif function_code == "P" or function_code == "p":
+        list_all_items()
+
+    # Exit loop
+    elif function_code == "Q" or function_code == "q":
+        # This is where we want to stop our loop
+        return False
+
     else:
-        print("Unknown Option")
 
-def test():
-    # create("purple sox")
-    # create("red cloak")
+        print("Invalid Input")
+    return True
 
-    # print(read(0))
-    # print(read(1))
+def user_input(prompt):
+    # the input function will display a message in the terminal
+    # and wait for user input.
+    user_input = input(prompt)
+    return user_input
+    user_value = user_input("Please Enter a value: ")
+    print(user_value)
 
-    # update(0, "purple socks")
 
-    # destroy(1)
+# def test():
+#     # Your testing code here
+#     # ...
+#     # Call your new function with the appropriate value
+#     # select("C")
+#     # View the results
+#     # list_all_items()
+#     # Call function with new value
+#     # select("R")
+#     # View results
+#     # list_all_items()
+#     # Continue until all code is run
+#     # list_all_items()
 
-    # print(read(0))
 
-    # list_all_items()
 
-test()
+running = True
+while running:
+    selection = user_input(
+        "Press C to Add to list, D to Remove from list, U to Update item, M to mark as Complete, and P to Show list. Press Q to Quit. ")
+    running = select(selection)
