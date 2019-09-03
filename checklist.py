c2886da412ad54d2db3df7d51606844856dc9e41
MondaleFelix <mondalefelix@yahoo.com>
2019-09-02 19:09:10 -0700

    added tests

--- checklist.py
+++ checklist.py
@@ -27,4 +27,40 @@ def mark_completed(index):
 
     # Add code here that marks an item as completed
 
+def select(function_code):
+    # Create item
+    if function_code == "C":
+        input_item = user_input("Input item:")
+        create(input_item)
+
+    # Read item
+    elif function_code == "R":
+        item_index = user_input("Index Number?")
+
+        # Remember that item_index must actually exist or our program will crash.
+        read(item_index)
+
+    # Print all items
+    elif function_code == "P":
+        list_all_items()
+
+    # Catch all
+    else:
+        print("Unknown Option")
+
 def test():
+    # create("purple sox")
+    # create("red cloak")
+
+    # print(read(0))
+    # print(read(1))
+
+    # update(0, "purple socks")
+
+    # destroy(1)
+
+    # print(read(0))
+
+    # list_all_items()
+
+test()
